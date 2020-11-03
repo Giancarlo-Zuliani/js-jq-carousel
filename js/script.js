66f91fb53a0d53dfb4c5cb4a6ebc981fa86f8050
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-03 16:46:49 +0100

    click on dots now work proprerly

--- js/script.js
+++ js/script.js
@@ -1,49 +1,59 @@
 
 var nextBtn = $('.next');
 var prevBtn = $('.prev');
-
+var dots = $('.nav i');
 
 $(document).ready(function(){
 
   nextBtn.click(carouselNext);
   prevBtn.click(carouselPrev);
 
-});
+  $(document).keydown(function(evt){
+    if(evt.keyCode === 37){
+      carouselPrev();
+    }
+    else if(evt.keyCode === 39){
+      carouselNext();
+    }
+    evt.preventDefault();
+  })
 
-$(document).keydown(function(evt){
-  if(evt.keyCode === 37){
-    carouselPrev();
-  }
-  else if(evt.keyCode === 39){
-    carouselNext();
-  }
-  evt.preventDefault();
-})
+  dots.click(function(){
+    var activeImg = $('.images .active');
+    var activeDot = $('.nav .active');
+    activeImg.removeClass('active');
+    activeDot.removeClass('active');
+    this.classList.add('active');
+    var index = dots.index(this);
+    var imgArray = $('.images img');
+    imgArray[index].classList.add('active');
+  })
+});
 
 
 function carouselNext(){
   var activeImg = $('.images .active');
-  var activeIndex = $('.nav .active')
-  if(activeImg.hasClass('last') && activeIndex.hasClass('last')){
+  var activeDot = $('.nav .active');
+  if(activeImg.hasClass('last') && activeDot.hasClass('last')){
     activeImg.removeClass('active');
-    activeIndex.removeClass('active');
+    activeDot.removeClass('active');
     $('img.first').addClass('active');
     $('.nav .first').addClass('active');
   }else{
   activeImg.removeClass('active').next().addClass('active');
-  activeIndex.removeClass('active').next().addClass('active');
+  activeDot.removeClass('active').next().addClass('active');
   }
 };
 
 function carouselPrev(){
   var activeImg = $('.images .active');
-  var activeIndex = $('.nav .active');
-  if(activeImg.hasClass('first') && activeIndex.hasClass('first')){
+  var activeDot = $('.nav .active');
+  if(activeImg.hasClass('first') && activeDot.hasClass('first')){
     activeImg.removeClass('active');
-    activeIndex.removeClass('active');
+    activeDot.removeClass('active');
     $('img.last').addClass('active');
     $('.nav .last').addClass('active');
   }
   activeImg.removeClass('active').prev().addClass('active');
-  activeIndex.removeClass('active').prev().addClass('active');
-}
+  activeDot.removeClass('active').prev().addClass('active');
+};
