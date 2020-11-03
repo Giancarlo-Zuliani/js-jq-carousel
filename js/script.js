38989abd54c150dd5a3fcba09fc91529c0a332fc
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-03 14:28:34 +0100

    carousel arrow work proprerly

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+var nextBtn = $('.next');
+var prevBtn = $('.prev');
+
+
+$(document).ready(function(){
+
+  nextBtn.click(carouselNext);
+  prevBtn.click(carouselPrev);
+
+});
+
+function carouselNext(){
+  var activeImg = $('.images .active');
+  var activeIndex = $('.nav .active')
+  if(activeImg.hasClass('last') && activeIndex.hasClass('last')){
+    activeImg.removeClass('active');
+    activeIndex.removeClass('active');
+    $('img.first').addClass('active');
+    $('.nav .first').addClass('active');
+  }else{
+  activeImg.removeClass('active').next().addClass('active');
+  activeIndex.removeClass('active').next().addClass('active');
+  }
+};
+
+function carouselPrev(){
+  var activeImg = $('.images .active');
+  var activeIndex = $('.nav .active');
+  if(activeImg.hasClass('first') && activeIndex.hasClass('first')){
+    activeImg.removeClass('active');
+    activeIndex.removeClass('active');
+    $('img.last').addClass('active');
+    $('.nav .last').addClass('active');
+  }
+  activeImg.removeClass('active').prev().addClass('active');
+  activeIndex.removeClass('active').prev().addClass('active');
+}
